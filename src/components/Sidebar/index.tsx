603a6b3b4239c87903688e5fe60d46dfe63958ad
Pablo Wesley <wesley_pablo68@yahoo.com>
2021-04-14 21:01:14 -0300

    feat(frontend): update Sidebar styles and interactions

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,36 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {
-  List, ListItem, ListItemText, MuiThemeProvider, Drawer
-} from '@material-ui/core'
-
-import { StyledSidebar } from './styles'
+import React, { useState } from 'react'
+import { Link, Route, Switch, Redirect, useLocation } from 'react-router-dom'
+import { List } from '@material-ui/core'
+import { IconBaseProps } from 'react-icons/lib/cjs'
+import { PageTemplate } from '@screens/template'
+import { RoutesScreen, StyledSidebar, StyledListItem, StyledTooltip } from './styles'
 
 export interface ITask {
+  id: string
   title?: string
   href?: string
-  component?(): JSX.Element
   icon?: any
+  component?(): JSX.Element
   onClick?(): void
 }
 
 export interface ISidebar {
-  tasks: ITask[]
+  main?: ITask[]
+  extras?: ITask[]
+  tasks?: ITask[]
 }
 
-export default function Sidebar({ tasks } : ISidebar) {
+function MenuIcon({ id, icon, ...props } : ITask & IconBaseProps) {
+  return (
+    React.createElement(icon || 'div', props)
+  )
+}
+
+export function Routes({ main, extras } : ISidebar) {
+  const routes = [...main, ...extras]
+
+  return (
+    <RoutesScreen>
+      <Switch>
+        {routes.map(({ href, component }, index) => (
+          <Route exact path={href || '?'} component={component || PageTemplate} key={`${index.toString()}`} />
+        ))}
+        <Redirect from="/" to="projects" />
+      </Switch>
+    </RoutesScreen>
+  )
+}
+
+export default function Sidebar({ main, extras } : ISidebar) {
+  const { pathname } = useLocation()
+  const [selected, setSelected] = useState(pathname)
+
+  const handleListItemClick = (href: string, onClick?: () => void) => {
+    if (href === undefined && typeof onClick === 'function') onClick()
+    else setSelected(href || selected)
+  }
+
+  const ActivityList = ({ tasks } : ISidebar) => (
+    <List>
+      {tasks.map(({ id, title, href, icon, onClick }, index) => (
+        <StyledTooltip title={title} key={index.toString()}>
+          <StyledListItem selected={selected === href} component={Link} to={href || '?'} key={index.toString()} onClick={() => handleListItemClick(href, onClick)}>
+            {icon === undefined ? '*' : <MenuIcon id={id} icon={icon} size={28} />}
+          </StyledListItem>
+        </StyledTooltip>
+      ))}
+    </List>
+  )
+
   return (
     <StyledSidebar>
-      <List>
-        {tasks.map(({
-          title, href, icon, onClick
-        }, index) => (
-          <ListItem button component={Link} to={href} key={index.toString()} onClick={onClick}>
-            {icon}
-            <ListItemText primary={title} />
-          </ListItem>
-        ))}
-      </List>
+      <ActivityList tasks={main} />
+      <ActivityList tasks={extras} />
     </StyledSidebar>
   )
 }
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,21 +1,77 @@
-import { createStyles, withStyles } from '@material-ui/core/styles'
-import { Drawer } from '@material-ui/core'
+import styled from 'styled-components'
+import { Drawer, ListItem, Tooltip, withStyles } from '@material-ui/core'
+import { baseFeatures } from '@constants/styles'
 
-import { titlebarHeight } from '@components/Titlebar/styles'
+const baseColorPallete = {
+  background: '#343746',
+  icon: '#6272A4',
+  workspace: '#989898',
+  status: '#191A21',
+  selected: '#F8F8F2'
+}
 
-const drawerWidth = 120
+export const RoutesScreen = styled.div`
+  display: grid;
+  box-sizing: border-box;
+  background-color: #989898;
+  width: 100%;
+  max-height: calc(100% - ${baseFeatures.titlebarHeight + baseFeatures.statusbarHeight}px);
+  margin-top: ${baseFeatures.titlebarHeight}px;
+  margin-bottom: ${baseFeatures.statusbarHeight}px;
+  padding: 10px;
+`
 
-const SidebarMuiStyles = createStyles({
-  root: {
-    flexShrink: 0,
-    width: drawerWidth
-  },
-  paper: {
-    top: titlebarHeight,
-    width: drawerWidth
+export const StyledSidebar: typeof Drawer = styled(Drawer)`
+  .MuiDrawer-root {
+    flex-shrink: 0;
+    width: ${baseFeatures.drawerWidth}px;
   }
-})
 
-const StyledSidebar = withStyles(SidebarMuiStyles)(Drawer)
+  .MuiDrawer-paper {
+    position: relative;
+    top: ${baseFeatures.titlebarHeight}px;
+    width: ${baseFeatures.drawerWidth}px;
+    height: calc(100% - ${baseFeatures.titlebarHeight + baseFeatures.statusbarHeight}px);
+    background-color: ${baseColorPallete.background};
+    justify-content: space-between;
+  }
+`
 
-export { StyledSidebar }
+export const StyledListItem: typeof ListItem = styled(ListItem).attrs({
+  button: true,
+  disableGutters: true
+})`
+  &.MuiListItem-root {
+    max-width: 100%;
+    height: ${baseFeatures.drawerWidth}px;
+    margin-bottom: 4px;
+    justify-content: center;
+    margin-top: auto;
+  }
+  
+  &.MuiListItem-button {
+    color: ${baseColorPallete.icon};
+    background-color: ${baseColorPallete.background};
+ 
+    :hover {
+      color: ${baseColorPallete.selected};
+      background-color: ${baseColorPallete.background};
+    }
+
+    &.Mui-selected {
+      &, :hover {
+      color: ${baseColorPallete.selected};
+      background-color: #F4F4F41A;
+      border-left: 2px solid #FF79C680;
+      }
+    }
+  }
+`
+export const StyledTooltip: typeof Tooltip = styled(withStyles({
+  tooltip: {
+    margin: 2
+  }
+})(Tooltip)).attrs({
+  placement: 'right',
+  enterDelay: 200
+})``
