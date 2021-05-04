34cdd5a26d29c65a96e5e48f0cadfd4193d689d8
Pablo Wesley <wesley_pablo68@yahoo.com>
2021-05-04 19:40:02 -0300

    test: update extension for tests

--- extensions/super-extension/super-extension.ts
+++ extensions/super-extension/super-extension.ts
@@ -7,12 +7,13 @@ simple.command('The Big One')
   .param(ParamsTypes.FLAG, '--Test', 'Meu Test', 0, [])
   .param(ParamsTypes.FLAG, '--Test', 'Meu Test', 0, [[InputTypes.ENUM, ['ConcordantPair', 'DiscordantPair']]])
   .param(ParamsTypes.FLAG, '--runThreadN', 'Threads', 1, [[InputTypes.NUMBER]], RequiredTypes.COMMON_OUT)
-  .param(ParamsTypes.FLAG, '--genomeDir', 'Genome', 1, [[InputTypes.DIR]])
-  .param(ParamsTypes.FLAG, '--readFilesIn', 'Read files', -1, [[InputTypes.FILE]])
+  .param(ParamsTypes.FLAG, '--genomeDir', 'Genome', 1, [[InputTypes.DIR]], RequiredTypes.MAIN_IN)
+  .param(ParamsTypes.FLAG, '--readFilesIn', 'Read files', -1, [[InputTypes.FILE]], RequiredTypes.MAIN_IN)
   .param(ParamsTypes.FLAG, '--alignEndsProtrude', 'Protrusion ends', 2, [
     [InputTypes.NUMBER],
     [InputTypes.ENUM, ['ConcordantPair', 'DiscordantPair']]
   ])
+  .param(ParamsTypes.ARG, 'out', 'My Output', -1, [[InputTypes.FILE]], RequiredTypes.MAIN_OUT)
   .end()
 
 simple.generateExtension('super-extension', __dirname)
