a716c40cb97cd170e3a3d0a44650bc64142f678a
Pablo Wesley <wesley_pablo68@yahoo.com>
2021-05-10 18:53:54 -0300

    feat: update workspace features menu

--- src/screens/workspace/index.tsx
+++ src/screens/workspace/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core'
 import { GiTestTubes } from 'react-icons/gi'
-import { IoMdAnalytics } from 'react-icons/io'
-import { SiNodeRed, SiGnubash } from 'react-icons/si'
+import { SiGooglesearchconsole, SiNodeRed, SiGnubash } from 'react-icons/si'
+
 import { GoMortarBoard, GoGear } from 'react-icons/go'
+import { CgArrowsExchange } from 'react-icons/cg'
 import { AiFillQuestionCircle } from 'react-icons/ai'
+import { HiClipboardCheck } from 'react-icons/hi'
 import { BaseTheme } from '@constants/themes'
 import { isDevelopment } from '@constants/environment'
 import { GlobalStyle } from '@constants/styles'
@@ -12,13 +14,15 @@ import Sidebar, { Routes, ISidebar } from '@components/Sidebar'
 import Titlebar from '@components/Titlebar'
 import SystemInfo from '@screens/systeminfo'
 import Pipeline from '@screens/pipeline'
+import Projects from '@screens/projects'
 import { WorkspaceLayout } from './styles'
 import { toggleDevTools } from './internals'
 
 const main: ISidebar['tasks'] = [
-  { id: 'projects', title: 'Projects', href: '/projects', icon: GiTestTubes },
-  { id: 'pipelines', title: 'Pipelines', href: '/pipelines', component: Pipeline, icon: SiNodeRed },
-  { id: 'analysis', title: 'Data Analysis', href: '/analysis', icon: IoMdAnalytics }
+  { id: 'hosts', title: 'Hosts', href: '', icon: CgArrowsExchange },
+  { id: 'projects', title: 'Projects', href: '/projects', icon: GiTestTubes, component: Projects },
+  { id: 'pipelines', title: 'Pipelines', href: '/pipelines', icon: SiNodeRed, component: Pipeline },
+  { id: 'analysis', title: 'Toolbox', href: '/analysis', icon: SiGooglesearchconsole }
 ]
 
 const extras: ISidebar['tasks'] = [
@@ -28,7 +32,7 @@ const extras: ISidebar['tasks'] = [
 
 if (isDevelopment) {
   main.push(
-    { id: 'sysinfo', title: 'System Info', icon: AiFillQuestionCircle, href: '/sysinfo', component: SystemInfo },
+    { id: 'sysinfo', title: 'System Info', href: '/sysinfo', icon: AiFillQuestionCircle, component: SystemInfo },
     { id: 'devtools', title: 'DevTools', icon: SiGnubash, onClick: toggleDevTools }
   )
 }
