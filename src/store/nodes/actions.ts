017ce4fb2ec8a9c98fc3d1f25f758fc11e3acc01
Pablo Wesley <wesley_pablo68@yahoo.com>
2021-05-17 14:16:53 -0300

    feat: update nodes and edges states management

--- src/store/nodes/actions.ts
+++ src/store/nodes/actions.ts
@@ -1,7 +1,9 @@
 import { action } from 'typesafe-actions'
-import { FlowElement } from 'react-flow-renderer'
+import { FlowElement, Edge, Connection } from 'react-flow-renderer'
+import { RibozillaNode } from '@screens/pipeline/internals'
 import { NodesActionTypes } from './types'
 
 export const addNode = (nodes: FlowElement) => action(NodesActionTypes.ADD_NODE, nodes)
-export const deleteNode = action(NodesActionTypes.DELETE_NODE)
+export const deleteNode = (id: string) => action(NodesActionTypes.DELETE_NODE, <RibozillaNode>{ id })
 export const loadNodes = action(NodesActionTypes.LOAD_NODES)
+export const linkNodes = (connection: Edge | Connection) => action(NodesActionTypes.LINK_NODES, connection)
--- src/store/nodes/reducers.ts
+++ src/store/nodes/reducers.ts
@@ -1,31 +1,30 @@
+/* eslint-disable no-case-declarations */
 import { Reducer, PayloadAction } from 'typesafe-actions'
-import { FlowElement } from 'react-flow-renderer'
+import { FlowElement, addEdge, Edge, Connection, removeElements } from 'react-flow-renderer'
 import { RibozillaNode } from '@screens/pipeline/internals'
-import { NodesState, NodesActionTypes, templateNode } from './types'
+import { NodesState, NodesActionTypes } from './types'
 
 const initialState: NodesState = {
-  nodes: [{
-    id: 'node-x',
-    type: 'software',
-    position: { x: 250, y: 50 },
-    data: {
-      software: 'Initial',
-      command: 'ribozi',
-      version: '3.0.0-beta'
-    }
-  }, templateNode]
+  nodes: []
 }
 
-const nodesReducer : Reducer<NodesState, PayloadAction<NodesActionTypes, RibozillaNode>> = (state = initialState, action) => {
+const nodesReducer : Reducer<NodesState, PayloadAction<NodesActionTypes, RibozillaNode & Edge>> = (state = initialState, action) => {
   switch (action.type) {
     case NodesActionTypes.ADD_NODE:
       return { ...state, nodes: [...state.nodes, action.payload] }
+
     case NodesActionTypes.DELETE_NODE:
-      console.log('delete')
-      return state
+      const deleteElements = state.nodes.filter(({ id }) => id === action.payload.id)
+      return { ...state, nodes: removeElements(deleteElements, state.nodes) }
+
     case NodesActionTypes.LOAD_NODES:
       console.log('load')
       return state
+
+    case NodesActionTypes.LINK_NODES:
+      console.log('link')
+      return { ...state, nodes: addEdge(action.payload as Edge, state.nodes) }
+
     default:
       return state
   }
